b9612ce5f13c103ee88344d1b85315b66edc73c3
Meesala Kavya Sri <meesalakavyasri@gmail.com>
2024-10-17 23:40:22 +0530

    Add files via upload

---
+++ code-3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package expense_tracker;
+
+/**
+ *
+ * @author anuja
+ */
+public class Expense_Tracker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new ExpensesIncomesTracker().setLocationRelativeTo(null);
+    }
+    
+}
